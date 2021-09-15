9f15c8dff2e97ee27851ead9adb850d269127657
pratiksh404 <pratikdai404@gmail.com>
2021-09-15 21:07:41 +0545

    detail bs property added

--- src/Helpers/NepaliDateHelper.php
+++ src/Helpers/NepaliDateHelper.php
@@ -3,7 +3,7 @@
 use Carbon\Carbon;
 use Pratiksh\Nepalidate\Services\NepaliDate;
 
-if (! function_exists('nepaliDate')) {
+if (!function_exists('nepaliDate')) {
     function nepaliDate(Carbon $date)
     {
         $mode = config('nepalidate.mode', 1);
@@ -19,23 +19,39 @@ if (! function_exists('nepaliDate')) {
     }
 }
 
-if (! function_exists('toBS')) {
+if (!function_exists('toBS')) {
     function toBS(Carbon $date)
     {
         return (new NepaliDate())->create($date)->toBS();
     }
 }
 
-if (! function_exists('toFormattedBSDate')) {
+if (!function_exists('toFormattedBSDate')) {
     function toFormattedBSDate(Carbon $date)
     {
         return (new NepaliDate())->create($date)->toFormattedBSDate();
     }
 }
 
-if (! function_exists('toFormattedNepaliDate')) {
+if (!function_exists('toFormattedNepaliDate')) {
     function toFormattedNepaliDate(Carbon $date)
     {
         return (new NepaliDate())->create($date)->toFormattedNepaliDate();
+    }
+}
+
+if (!function_exists('toDetailBS')) {
+    function toDetailBS(Carbon $date)
+    {
+        $bs_array = (new NepaliDate())->create($date)->toBSArray();
+        $to_detail_bs = null;
+        if (!is_null($bs_array)) {
+            if (is_array($bs_array)) {
+                if (count($bs_array) > 0) {
+                    $to_detail_bs = json_decode(json_encode($bs_array));
+                }
+            }
+        }
+        return $to_detail_bs;
     }
 }
